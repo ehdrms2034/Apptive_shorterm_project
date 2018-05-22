ef6916acc33527bd20071762d83617397caf1102
JangYuri <cyrjang@naver.com>
2018-05-23 07:35:22 +0900

    yuri_0523

--- RGB/app/src/main/java/com/example/kimdonggeun/rgb/MainActivity.java
+++ RGB/app/src/main/java/com/example/kimdonggeun/rgb/MainActivity.java
@@ -1,30 +1,44 @@
 package com.example.kimdonggeun.rgb;
 
-import android.app.DialogFragment;
 import android.content.Intent;
-import android.support.v4.app.FragmentManager;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    ImageButton btnGraph;
+    ViewPager container;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btnGraph = findViewById(R.id.btn_graph);
+        btnGraph.setOnClickListener(this);
 
-        Button btn = (Button)findViewById(R.id.popup_btn);
-        btn.setOnClickListener(new Button.OnClickListener(){
-            @Override
-            public void onClick(View view){
-                Intent forms = new Intent(MainActivity.this,form.class);
-                startActivity(forms);
+        container = findViewById(R.id.content_frame);
+        Main_PagerAdapter adapter = new Main_PagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new Main0_Start());
+        adapter.addFragment(new Main1_Graph());
+        container.setAdapter(adapter);
 
-            }
-        });
+    }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_graph:
+                Toast.makeText(this, "heheh", Toast.LENGTH_LONG);
+                Intent intent = new Intent(this, GraphActivity.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
     }
 }
