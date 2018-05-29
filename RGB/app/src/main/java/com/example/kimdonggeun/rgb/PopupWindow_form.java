0303b2ae52a7ce420fdc7fc02765244df95ff0c5
ehdrms2034 <ehdrms2034@naver.com>
2018-05-29 23:30:17 +0900

    1
    11

--- RGB/app/src/main/java/com/example/kimdonggeun/rgb/PopupWindow_form.java
+++ RGB/app/src/main/java/com/example/kimdonggeun/rgb/PopupWindow_form.java
@@ -2,15 +2,26 @@ package com.example.kimdonggeun.rgb;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.SeekBar;
 
 
 public class PopupWindow_form extends AppCompatActivity {
 
+    SeekBar R_seekbar;
+    SeekBar G_seekbar;
+    SeekBar B_seekbar;
+
+
+    ViewPager viewpager;
+
+    boolean threaddead;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,18 +33,50 @@ public class PopupWindow_form extends AppCompatActivity {
         ImageButton exit_btn = (ImageButton)findViewById(R.id.form_exit_btn);
         exit_btn.setOnClickListener(new Button.OnClickListener(){
             public void onClick(View view){
+            threaddead = true;
             finish();
+
             }
         });
+        //ex
+        R_seekbar = (SeekBar)findViewById((R.id.form_seekbar1));
+        G_seekbar = (SeekBar)findViewById((R.id.form_seekbar2));
+        B_seekbar = (SeekBar)findViewById((R.id.form_seekbar3));
+        //R_seekbar.setEnabled(false);
+        //G_seekbar.setEnabled(false);
+        //B_seekbar.setEnabled(false);
+        //R_seekbar.setProgress(5);
+
 
         //viewpager
 
-        ViewPager viewpager = (ViewPager)view.findViewById(R.id.form_view_pager);
+        viewpager = (ViewPager)view.findViewById(R.id.form_view_pager);
         viewpager.setAdapter(new PopupWindow_viewpageradapter(getSupportFragmentManager()));
+
+
+
 
 
     }
 
+    public void addR_seekbar(int i){
+        R_seekbar.setProgress(R_seekbar.getProgress()+i);
+    }
+
+    public void addG_seekbar(int i){
+        G_seekbar.setProgress(G_seekbar.getProgress()+i);
+    }
+    public void addB_seekbar(int i){
+        B_seekbar.setProgress(B_seekbar.getProgress()+i);
+    }
+
+    public void next_viewpage(){
+        viewpager.setCurrentItem(viewpager.getCurrentItem()+1);
+    }
+
+    public void previous_viewpage(){
+        viewpager.setCurrentItem(viewpager.getCurrentItem()-1);
+    }
 
 
 
